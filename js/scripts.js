7d225dbb59d0f4c295c228e937793eb36fa1d42d
Bogna Konstanty <rosiakb@gmail.com>
2016-12-30 09:43:40 +0100

    remove brackets

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 var greenElephants = 'Zielone słonie';
 var greenElephantsUpperCase = greenElephants.toUpperCase();
-var textCharsAfter = text.replace('Papugi', [greenElephantsUpperCase]);
-var partOfText = textCharsAfter.slice(0,[textCharsAfter.length/2]);
+var textCharsAfter = text.replace('Papugi', greenElephantsUpperCase);
+var partOfText = textCharsAfter.slice(0,textCharsAfter.length/2);
 console.log(partOfText);
